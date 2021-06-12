24105b812ce90c599c10de2b637e27ee12c33fd6
LuxSalgado <lasalgado.14@est.ucab.edu.ve>
2021-06-12 16:39:35 +0000

    Version 3.0: Cambios de diseño para mostrar los numeros digitales

--- src/js/component/secondsCounter.js
+++ src/js/component/secondsCounter.js
@@ -4,7 +4,7 @@ import PropTypes, { object } from "prop-types";
 export function SecondsCounter(props) {
 	return (
 		<div
-			className="rounded display-2 shadow d-flex align-items-center justify-content-center mx-2 digito"
+			className="rounded display-2 d-flex align-items-center justify-content-center mx-2 digito"
 			id={props.identificador}>
 			{props.seconds}
 		</div>
